02546288c551d611fadabf63f0f32045279101ca
Jackie <jackieluc17@gmail.com>
2017-10-23 22:52:26 -0600

    - Working navigation header - move old legacy static html files to old_static folder for reference - update public/index.html to include more other css files

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -1,49 +1,49 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom'
-// import styles from '../../public/assets/css/styles.css'
-// import nav from '../../public/assets/css/Navigation-Clean1.css'
-import bootstrap from 'react-bootstrap'
 
 class Navigation extends Component {
   render() {
     return (
       <Router>
-        <div> 
+        <div className='header'> 
           <nav className='navbar navbar-default navigation-clean'>
-            <div className='container'>
-                <div className='navbar-header'>
-                  <a className='navbar-brand navbar-link' href='.'><img src='assets/img/Logo.png' id='logo' title='logo' />
-                    <button className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navcol-1'><span className='sr-only'>Toggle navigation</span>
-                    <span className='icon-bar'></span><span className='icon-bar'></span><span className='icon-bar'></span></button>
-                  </a>
-                <div className='collapse navbar-collapse' id='navcol-1'>
-                    <ul className='nav navbar-nav navbar-right'>
-                        <li className='dropdown'><a className='dropdown-toggle' data-toggle='dropdown' aria-expanded='false' >About us<span className='caret'></span></a>
-                            <ul className='dropdown-menu' role='menu'>
-                                <li role='presentation'><a href='/about-us/vision/'>Our Vision</a></li>
-                                <li role='presentation'><a href='/about-us/team/'>Our Team</a></li>
-                                <li role='presentation'><a href='/about-us/constitution/'>Our Constitution</a></li>
-                                <li role='presentation'><a href='../about-us/faq/'>FAQ</a></li>                                
-                            </ul>
-                        </li>
-                        <li role='presentation'><a href='schools/'>For Schools</a></li>
-                        <li role='presentation'><a href='parents/'>For Parents</a></li>
-                        <li role='presentation'><a href='blog/'>Blog</a></li>
-                        <li role='presentation'><a href='get-involved/'>Get Involved</a></li>
-                        <li role='presentation' id='donate'><a href='donate/'>Donate</a></li>
-                        <li role='presentation' ><NavLink to='/test'>Test</NavLink></li>
-                    </ul>
-                </div>
+              <div className='container'>
+                  <div className='navbar-header'>
+                    <a className='navbar-brand navbar-link'>
+                      <NavLink to='/'><img src='/assets/img/Logo.png' id='logo' title='logo' /></NavLink>
+                    </a>
+                    <button className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navcol-1'>
+                      <span className='sr-only'>Toggle navigation</span>
+                      <span className='icon-bar'></span><span className='icon-bar'></span><span className='icon-bar'></span>
+                    </button>
+                  </div>
+                  <div className='collapse navbar-collapse' id='navcol-1'>
+                      <ul className='nav navbar-nav navbar-right'>
+                          <li className='dropdown'><a className='dropdown-toggle' data-toggle='dropdown' aria-expanded='false' href='#'>About us<span className='caret'></span></a>
+                              <ul className='dropdown-menu' role='menu'>
+                                  <li role='presentation'><a href='/about-us/vision/'>Our Vision</a></li>
+                                  <li role='presentation'><a href='/about-us/team/'>Our Team</a></li>
+                                  <li role='presentation'><a href='/about-us/constitution/'>Our Constitution</a></li>
+                              </ul>
+                          </li>
+                          <li role='presentation'><a href='schools/'>For Schools</a></li>
+                          <li role='presentation'><a href='parents/'>For Parents</a></li>
+                          <li role='presentation'><a href='blog/'>Blog</a></li>
+                          <li role='presentation'><a href='get-involved/'>Get Involved</a></li>
+                          <li role='presentation' id='donate'><a href='donate/'>Donate</a></li>
+                          <li role='presentation' ><NavLink to='/test'>Test</NavLink></li>
+                      </ul>
+                  </div>
               </div>
-            </div>
           </nav>
           <div id='spacer'></div>
-          <div id='spacer'></div>
+          <main>
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/test' component={Test} />
             <Route path='*' component={NotFound} />
           </Switch>
+          </main>
         </div>
       </Router>
     )
@@ -51,9 +51,7 @@ class Navigation extends Component {
 }
 const Home = () => {
   return(
-    <main>
       <h2>Hello from Home!</h2>
-    </main>
   )
 }
 const Test = () => {
--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import '../../node_modules/bootstrap/dist/css/bootstrap.css'
-import '../public/assets/css/styles.css'
-import '../public/assets/css/Navigation-Clean1.css'
 import IndexPage from './pages/IndexPage.js'
 
 ReactDOM.render(<IndexPage />, document.getElementById('main-content'));
