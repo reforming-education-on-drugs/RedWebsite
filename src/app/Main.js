97bd83f121284fa534fcccdffa19e676a1711fda
Jackie <jackieluc17@gmail.com>
2017-10-24 19:22:21 -0600

    refactor main.js to be the main entry point of the program

--- src/app/Main.js
+++ src/app/Main.js
@@ -1,5 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import Index from './pages/Index.js'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './components/Header'
+import IndexPage from './pages/IndexPage'
+import NotFoundPage from './pages/NotFoundPage'
 
-ReactDOM.render(<Index />, document.getElementById('main-content'));
+class Main extends Component {
+  render() {
+    return (
+      <Router>
+        <div> 
+          <Header />
+          <Spacer />
+          <Switch>
+            <Route exact path='/' component={ IndexPage } />
+            <Route path='*' component={ NotFoundPage } />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
+
+class Spacer extends Component {
+  // Spacer required to separate main content from navigation bar
+  render() { return( <div id='spacer' /> ) }
+}
+
+ReactDOM.render(<Main />, document.getElementById('main-content'));
