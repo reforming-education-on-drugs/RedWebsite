17c15990cb0348fd121ad7b16d0f0df8f88bfeaf
Jackie <jackieluc17@gmail.com>
2017-10-29 23:52:32 -0600

    Added swiper (react version) and description to home index page

--- src/app/pages/IndexPage.js
+++ src/app/pages/IndexPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Swiper from 'react-id-swiper'
 
 class IndexPage extends Component {
   render() {
@@ -7,8 +8,62 @@ class IndexPage extends Component {
 
     return(
       <main>
-        <h2>Hello from home!</h2>
+        <SwiperSlider />
+        <Description />
+        {/* TODO: Testimonials */}
+        {/* TODO: MailChimp Sign Up form */}
+        {/* TODO: Brands */}
+        {/* TODO: Footer */}
       </main>
+    )
+  }
+}
+
+class SwiperSlider extends Component {
+  render() {
+
+    const swiperParams = {
+      slidesPerView: 1,
+      autoplay: {
+        delay: 5000,
+      },
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    }
+
+    return (
+      <div className="container" id="swiper">
+        <div className="simple-slider">
+          <Swiper {...swiperParams}>
+            <div style={{backgroundImage: 'url(/assets/img/main10.jpg)'}} />
+            <div style={{backgroundImage: 'url(/assets/img/main3.jpg)'}} />
+            <div style={{backgroundImage: 'url(/assets/img/main5.jpg)'}} />
+            <div style={{backgroundImage: 'url(/assets/img/main8.jpg)'}} />
+            <div style={{backgroundImage: 'url(/assets/img/main9.jpg)'}} />
+          </Swiper>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Description extends Component {
+  render() {
+
+    return (
+      <div className="coloredcontainer">
+        <div className="container">
+          <h1 className="text-center">Reforming Education on Drugs</h1>
+          <p>RED’s mission is to establish within students a foundational understanding of the biological mechanisms involved in illicit drug use and substance abuse. Through the use of interactive in-class presentations and demonstrations, we provide individuals with the ability to engage and think critically with these topics, all the while developing a love for science learning.</p>
+        </div>
+      </div>
     )
   }
 }
