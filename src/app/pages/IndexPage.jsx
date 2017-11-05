20178012be1ffa800c959d5778e0fc74c60361c9
Jackie <jackieluc17@gmail.com>
2017-11-05 01:43:54 -0600

    Fix nav styling

--- src/app/pages/IndexPage.jsx
+++ src/app/pages/IndexPage.jsx
@@ -45,7 +45,7 @@ function Description() {
       <div className="container">
         <h1 className="text-center">Reforming Education on Drugs</h1>
         { // eslint-disable-next-line
-        }<p>RED’s mission is to establish a foundational understanding of the biological mechanisms involved in illicit drug use and substance abuse. With interactive in-className presentations and demonstrations, we provide students with the ability to engage and think critically with these topics, all while developing a love for learning science.</p>
+        }<p>RED’s mission is to establish a foundational understanding of the biological mechanisms involved in illicit drug use and substance abuse. With interactive in-class presentations and demonstrations, we provide students with the ability to engage and think critically with these topics, all while developing a love for learning science.</p>
       </div>
     </div>
   );
