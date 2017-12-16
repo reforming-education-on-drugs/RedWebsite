d09ba83ea702bc4c2e9673216bfebb7c71fd9394
Jackie <jackieluc17@gmail.com>
2017-12-15 23:58:58 -0700

    #32 - add page not found with styling

--- src/app/pages/NotFoundPage.jsx
+++ src/app/pages/NotFoundPage.jsx
@@ -4,8 +4,13 @@ export default function NotFoundPage() {
   document.title = "RED | Page Not Found";
 
   return (
-    <main>
-      <h2>404 Page not found!</h2>
+    <main className="text-center">
+      <img src="../assets/img/symbol.png" width="50px" height="50px" style={{ margin: "2em 0" }} alt="RED logo symbol" />
+      <div className="container redcontainer">
+        <h2>Oops! Page not found!</h2>
+        <p>We're sorry, the page you requested was not found.</p>
+        <p>Visit our <a href="http://www.rededucate.com/">home page</a> or any of the navigation links above.</p>
+      </div>
     </main>
   );
 }
