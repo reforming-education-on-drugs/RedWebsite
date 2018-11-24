a11868f443f3e5d9ec08187b37ded0143da773ee
Jackie Luc <jackieluc17@gmail.com>
2018-11-24 10:45:52 -0700

    Fix analytics problem

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,6 +1,6 @@
 import ReactGA from 'react-ga';
 
-if (window.location.hostname === 'rededucate') {
+if (window.location.hostname === 'www.rededucate.com') {
   ReactGA.initialize('UA-105365789-1');
   ReactGA.pageview(window.location.pathname + window.location.search);
 }
