7f917ea602144e26a422f8e91978538bd3e08b41
Jackie Luc <jackieluc17@gmail.com>
2019-01-08 20:06:31 -0700

    include gotrue-api

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,27 +1,41 @@
 import React from "react";
+import GoTrue from 'gotrue-js';
 import Input from "../components/Input";
 
-export default function LoginPage() {
-  document.title = "RED | Login";
+const auth = new GoTrue({
+  APIUrl: "https://reducalgary.netlify/identity",
+  audience: "",
+  setCookie: false
+});
 
-  return (
-    <main>
-      <div className="login">
-        <div className="login-card">
-          <img src={require('../assets/images/Logo.png')} alt="RED logo symbol" />
-          <form>
-            <Input label="Email" type="email" />
-            <Input label="Password" type="password" />
-          </form>
-          <div className="password-reset">
-            <a href="https://www.netlify.com/" target="_blank" rel="noopener noreferrer">Forgot your password?</a>
+
+
+export default class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    document.title = "RED | Login";
+  }
+
+  render() {
+    return (
+      <main>
+        <div className="login">
+          <div className="login-card">
+            <img src={require('../assets/images/Logo.png')} alt="RED logo symbol" />
+            <form name="login">
+              <Input label="Email" type="email" />
+              <Input label="Password" type="password" />
+              <div className="password-reset">
+                <a href="https://www.netlify.com/" target="_blank" rel="noopener noreferrer">Forgot your password?</a>
+              </div>
+              <button type="submit">Sign in</button>
+            </form>
           </div>
-          <button>Sign in</button>
+          <div className="disclaimer">
+            <p><span>Don't have an account?</span>&nbsp;You need to become a member before you can sign up to volunteer.</p>
+          </div>
         </div>
-        <div className="disclaimer">
-          <p><span>Don't have an account?</span>&nbsp;You need to become a member before you can sign up to volunteer.</p>
-        </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
