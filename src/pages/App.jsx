ce27fd5b748795e425e8ef6fb164ccb564937777
Kourosh Banaeianzadeh <kourosh@synopsys.com>
2019-01-10 13:47:14 -0700

    Example of front end

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -15,6 +15,7 @@ import GetInvolvedPage from "./GetInvolvedPage";
 import DonatePage from "./DonatePage";
 import NotFoundPage from "./NotFoundPage";
 import ContactUsPage from "./ContactUsPage";
+import Gsheet from "./Gsheet";
 import Footer from "../components/Footer";
 
 // This is a class-based component because the current
@@ -40,6 +41,7 @@ class App extends React.Component {
           <Route exact path="/donate" component={DonatePage} />
           <Route exact path="/booking" component={BookingPage} />
           <Route exact path="/contact-us" component={ContactUsPage} />
+          <Route exact path="/gsheet" component={Gsheet} />
           <Route exact path="/faq" component={NotFoundPage} />
           <Route path="*" component={NotFoundPage} />
         </Switch>
---
+++ src/pages/Gsheet.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function Gsheet() {
+  document.title = "RED | Vision";
+
+  return (
+    <main>
+      <div className="container">
+        <h1>Google Sheet Data</h1>
+
+        {callGetPresentation({user:{email:"kouroshb26@gmail.com"}})}
+      </div>
+
+
+    </main>
+  );
+}
+
+
+
+function callGetPresentation(data) {
+  return fetch('/.netlify/functions/getPresentations', {
+    body: JSON.stringify(data),
+    method: 'POST'
+  }).then(response => {
+    console.log(response.json());
+  }).catch(error => console.log(error));
+}
+
+
+
+
+
+
