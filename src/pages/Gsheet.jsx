6a6df106903ed0cf4dd99582895df087743349b1
Kourosh Banaeianzadeh <kourosh@synopsys.com>
2019-01-16 16:46:32 -0700

    Example Frontend getting data

--- src/pages/Gsheet.jsx
+++ src/pages/Gsheet.jsx
@@ -1,32 +1,21 @@
 import React from "react";
+import Presentations from "../components/Presentations";
 
 export default function Gsheet() {
   document.title = "RED | Vision";
-  callGetPresentation({user:{email:"kouroshb26@gmail.com"}});
 
   return (
     <main>
       <div className="container">
         <h1>Google Sheet Data</h1>
+        <Presentations/>
       </div>
-
-
     </main>
   );
 }
 
 
 
-function callGetPresentation(data) {
-  return fetch('/.netlify/functions/getPresentations', {
-    body: JSON.stringify(data),
-    method: 'POST',
-  }).then(function(response) {
-    return response.json();
-  }).then(function(data) {
-    console.log(data);  // { "userId": 1, "id": 1, "title": "...", "body": "..." }
-  });
-}
 
 
 
