2077ead60dbaede52b81b4b1d893d33500375288
Jackie Luc <jackieluc17@gmail.com>
2019-01-19 20:06:37 -0700

    Update login page state

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,30 +1,39 @@
 import React from "react";
-import GoTrue from 'gotrue-js';
 import Input from "../components/Input";
-
-const auth = new GoTrue({
-  APIUrl: "https://reducalgary.netlify/identity",
-  audience: "",
-  setCookie: false
-});
-
-
 
 export default class LoginPage extends React.Component {
   constructor(props) {
     super(props);
     document.title = "RED | Login";
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    
+    this.setState({
+      [name]: target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
   }
 
   render() {
     return (
       <main>
-        <div className="login">
-          <div className="login-card">
+        <div className="identity">
+          <div className="identity-card">
             <img src={require('../assets/images/Logo.png')} alt="RED logo symbol" />
             <form name="login">
-              <Input label="Email" type="email" />
-              <Input label="Password" type="password" />
+              <Input name="email" label="Email" type="email" onChange={this.handleInputChange} />
+              <Input name="password" label="Password" type="password" onChange={this.handleInputChange} />
               <div className="password-reset">
                 <a href="https://www.netlify.com/" target="_blank" rel="noopener noreferrer">Forgot your password?</a>
               </div>
