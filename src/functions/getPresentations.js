5e76443626bf428647793ba98468816e774b55c5
Kourosh Banaeianzadeh <kourosh@synopsys.com>
2019-01-29 18:59:34 -0700

    fix complitation problem

--- src/functions/getPresentations.js
+++ src/functions/getPresentations.js
@@ -87,58 +87,58 @@ async function getPresentation(doc, email, presentationRow){
 
 
 
-function parseJwt (token) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  return JSON.parse(Buffer.from(base64, 'base64').toString());
-}
-
-
-function fetchUser(identity, id) {
-  class IdentityAPI {
-    constructor(apiURL, token) {
-      this.apiURL = apiURL;
-      this.token = token;
-    }
-
-    headers(headers = {}) {
-      return {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${this.token}`,
-        ...headers
-      };
-    }
-
-    parseJsonResponse(response) {
-      return response.json().then(json => {
-        if (!response.ok) {
-          return Promise.reject({ status: response.status, json });
-        }
-
-        return json;
-      });
-    }
-
-    request(path, options = {}) {
-      const headers = this.headers(options.headers || {});
-      return fetch(this.apiURL + path, { ...options, headers }).then(response => {
-        const contentType = response.headers.get("Content-Type");
-        if (contentType && contentType.match(/json/)) {
-          return this.parseJsonResponse(response);
-        }
-
-        if (!response.ok) {
-          return response.text().then(data => {
-            return Promise.reject({ stauts: response.status, data });
-          });
-        }
-        return response.text().then(data => {
-          data;
-        });
-      });
-    }
-  }
-
-  const api = new IdentityAPI(identity.url, identity.token);
-  return api.request(`/admin/users/${id}`);
-}
+// function parseJwt (token) {
+//   var base64Url = token.split('.')[1];
+//   var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+//   return JSON.parse(Buffer.from(base64, 'base64').toString());
+// }
+//
+//
+// function fetchUser(identity, id) {
+//   class IdentityAPI {
+//     constructor(apiURL, token) {
+//       this.apiURL = apiURL;
+//       this.token = token;
+//     }
+//
+//     headers(headers = {}) {
+//       return {
+//         "Content-Type": "application/json",
+//         "Authorization": `Bearer ${this.token}`,
+//         ...headers
+//       };
+//     }
+//
+//     parseJsonResponse(response) {
+//       return response.json().then(json => {
+//         if (!response.ok) {
+//           return Promise.reject({ status: response.status, json });
+//         }
+//
+//         return json;
+//       });
+//     }
+//
+//     request(path, options = {}) {
+//       const headers = this.headers(options.headers || {});
+//       return fetch(this.apiURL + path, { ...options, headers }).then(response => {
+//         const contentType = response.headers.get("Content-Type");
+//         if (contentType && contentType.match(/json/)) {
+//           return this.parseJsonResponse(response);
+//         }
+//
+//         if (!response.ok) {
+//           return response.text().then(data => {
+//             return Promise.reject({ stauts: response.status, data });
+//           });
+//         }
+//         return response.text().then(data => {
+//           data;
+//         });
+//       });
+//     }
+//   }
+//
+//   const api = new IdentityAPI(identity.url, identity.token);
+//   return api.request(`/admin/users/${id}`);
+// }
