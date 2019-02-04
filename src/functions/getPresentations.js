025b850e1b844c74a9efc3e312d18ea27d8b731a
Jackie Luc <jackieluc17@gmail.com>
2019-02-03 18:56:13 -0700

    Code clean up

--- src/functions/getPresentations.js
+++ src/functions/getPresentations.js
@@ -1,49 +1,58 @@
+/* eslint-disable no-console */
 const { promisify } = require('util');
-const {successResponse, errorResponse,authenticate,getSheetByName,convertPresentation,convertTime} = require('./presentationUtil');
+const {
+  successResponse,
+  errorResponse,
+  authenticate,
+  getSheetByName,
+  convertPresentation,
+  convertTime
+} = require('./presentationUtil');
 
 exports.handler = function(event, context, callback) {
   console.log('START: Received request.');
 
-  if(context.clientContext && context.clientContext.user && context.clientContext.user.email){
+  if (context.clientContext && context.clientContext.user && context.clientContext.user.email) {
     getPresentationForEmail(context.clientContext.user.email)
       .then(response => successResponse(callback,response))
       .catch(error => errorResponse(callback, error));
-  }else{
-    errorResponse(callback,"Unauthorized request. Please login in.")
   }
-
+  else{
+    errorResponse(callback, 'Unauthorized request. Please login in.');
+  }
 };
 
 
 //Curl command for testing
-//curl --header "Content-Type: application/json" --request POST --data @src/functions/payload.json localhost:9000/getPresentations
-// getPresentationForEmail("kouroshb26@gmail.com")
+//curl --header 'Content-Type: application/json' --request POST --data @src/functions/payload.json localhost:9000/getPresentations
+// getPresentationForEmail('kouroshb26@gmail.com')
 //   .then(response => successResponse(function(){},response))
 //   .catch(error => errorResponse(function(){}, error));
 
 
-async function getPresentationForEmail(email){
+async function getPresentationForEmail(email) {
   let response;
 
-  let doc = await authenticate();
-  let presentationSheet = await getSheetByName(doc,"Presentation"); //Master spread sheet denoting all presentations we have
+  const doc = await authenticate();
+  const presentationSheet = await getSheetByName(doc, 'Presentation'); //Master spread sheet denoting all presentations we have
 
-  let presentations =  await promisify(presentationSheet.getRows)({  //Get presentation information
+  const presentations = await promisify(presentationSheet.getRows)({  //Get presentation information
     offset: 1,
     limit: 100,
   });
 
   let promises = [];
-  for (let presentationRow of presentations){
+  for (let presentationRow of presentations) {
     //Don't process empty presentations
-    if(presentationRow.sheetname === "" || presentationRow.sheetname === null|| presentationRow.sheetname === "()"){
+    if (presentationRow.sheetname === '' || presentationRow.sheetname === null|| presentationRow.sheetname === '()') {
       continue;
     }
     //Don't show information about past presentations
-    if(new Date(presentationRow.date) < Date.now()){
+    if (new Date(presentationRow.date) < Date.now()) {
       continue;
     }
-    promises.push(getPresentation(doc,email,presentationRow));
+
+    promises.push(getPresentation(doc, email, presentationRow));
   }
 
   response = await Promise.all(promises); //Wait till all presentations are gathered
@@ -57,25 +66,26 @@ async function getPresentationForEmail(email){
 }
 
 
-async function getPresentation(doc, email, presentationRow){
+async function getPresentation(doc, email, presentationRow) {
 
   try {
-    let timeSheet = await getSheetByName(doc, presentationRow.sheetname); //Get sheet
+    const timeSheet = await getSheetByName(doc, presentationRow.sheetname); //Get sheet
 
-    let times = await promisify(timeSheet.getRows)({ //Get rows
+    const times = await promisify(timeSheet.getRows)({ //Get rows
       offset: 1,
       limit: 100,
     });
 
-    let presentation = convertPresentation(presentationRow); //Convert presentationRow to meet our API
+    const presentation = convertPresentation(presentationRow); //Convert presentationRow to meet our API
 
     for (let timeRow of times) {  //Convert the times to meet out API
       presentation.times.push(convertTime(timeRow, email));
     }
     return presentation;
 
-  }catch (e) {//Log errors
-    console.log(e);
+  }
+  catch (e) { //Log errors
+    console.error(e);
     return null;
   }
 }
--- src/functions/presentationUtil.js
+++ src/functions/presentationUtil.js
@@ -1,8 +1,8 @@
-let GoogleSpreadsheet = require('google-spreadsheet');
+/* eslint-disable no-console */
+const GoogleSpreadsheet = require('google-spreadsheet');
 const { promisify } = require('util');
 require('dotenv').config();
-const {spread_sheet_id , client_email, private_key} = process.env;
-
+const { spread_sheet_id, client_email, private_key } = process.env;
 
 exports.errorResponse = function(callback, err) {
   console.error('END: Error response.');
@@ -14,31 +14,37 @@ exports.errorResponse = function(callback, err) {
   });
 };
 
-exports.successResponse = function(callback, res){
+exports.successResponse = function(callback, res) {
   console.log('END: Success response.');
-  console.log("Returning data:");
+  console.log('Returning data:');
   console.log(JSON.stringify(res));
+
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(res)
   });
 };
 
-exports.authenticate = async function(){
-  let privateKeyWithNewline = private_key.replace(/\\n/g, '\n'); //Fix environment variable
+exports.authenticate = async function() {
+  const privateKeyWithNewline = private_key.replace(/\\n/g, '\n'); //Fix environment variable
 
-  const creds = {client_email,private_key:privateKeyWithNewline};
+  const creds = {
+    client_email,
+    private_key: privateKeyWithNewline
+  };
+
   const doc = new GoogleSpreadsheet(spread_sheet_id);
   await promisify(doc.useServiceAccountAuth)(creds);
+
   return doc;
 };
 
-exports.getSheetByName = async function(doc, name){
-  let info = await promisify(doc.getInfo)();
-  return info.worksheets.filter(function(worksheet){return name === worksheet.title;})[0];
+exports.getSheetByName = async function(doc, name) {
+  const info = await promisify(doc.getInfo)();
+  return info.worksheets.filter(worksheet => name === worksheet.title)[0];
 };
 
-exports.convertPresentation = function(presentationRow){
+exports.convertPresentation = function(presentationRow) {
   return {
     name: presentationRow.name,
     address: presentationRow.address,
@@ -48,7 +54,7 @@ exports.convertPresentation = function(presentationRow){
   };
 };
 
-exports.update = function(a,b){
+exports.update = function(a, b) {
   a.startTime = b.startTime;
   a.endTime = b.endTime;
   a.enrolled = b.enrolled;
@@ -56,21 +62,22 @@ exports.update = function(a,b){
   a.selected = b.selected;
 };
 
-exports.convertTime = function(timeRow,email){
-  let volunteers = timeRow.volunteers === "" ? [] : timeRow.volunteers.split(",");
+exports.convertTime = function(timeRow, email) {
+  let volunteers = timeRow.volunteers === '' ? [] : timeRow.volunteers.split(',');
   let time = {
     startTime: timeRow.starttime,
     endTime: timeRow.endtime,
     enrolled: volunteers.length,
-    capacity: parseInt(timeRow.capacity,10),
+    capacity: parseInt(timeRow.capacity, 10),
     selected: false, // Will be reset if the user is inside
   };
 
-  for (let user of volunteers){
-    if (email === user){
+  for (let user of volunteers) {
+    if (email === user) {
       time.selected = true;
       break;
     }
   }
+  
   return time;
 };
