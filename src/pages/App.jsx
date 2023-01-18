d46c4c56c2335b73b7d7865319055f7516662182
Ramin <ramin.kahidi@ucalgary.ca>
2023-01-18 14:41:00 -0700

    Should pass to confirmatino page when clicking on confirmation link

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -29,52 +29,49 @@ import Footer from "../components/Footer";
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
 // component at the top-level.
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Header />
-          <div id="spacer" />
-          <Switch>
-            <Route exact path="/" component={IndexPage} />
-            <Route exact path="/login" component={LoginPage} />
-            <Route path="/invite" component={InvitePage} />
-            <Route path="/confirmation" component={ConfirmationPage} />
-            <Route exact path="/about-us/vision" component={VisionPage} />
-            <Route exact path="/about-us/team" component={TeamPage} />
-            <Route
-              exact
-              path="/about-us/constitution"
-              component={ConstitutionPage}
-            />
-            <Route exact path="/schools" component={SchoolPage} />
-            <Route exact path="/parents" component={ParentPage} />
-            <Route exact path="/blog" component={BlogPage} />
-            <Route exact path="/get-involved" component={GetInvolvedPage} />
-            <Route exact path="/donate" component={DonatePage} />
-            <Route exact path="/old-booking" component={PresentationBooking} />
-            <Route
-              exact
-              path="/booking"
-              component={PresentationBookingWizard}
-            />
-            <Route exact path="/contact-us" component={ContactUsPage} />
-            <Route exact path="/volunteer" component={VolunteerPage} />
-            <Route exact path="/faq" component={NotFoundPage} />
-            <Route path="/password-reset" component={PasswordResetPage} />
-            <Route path="/recover" component={PasswordRecoveryPage} />
-            {/* <Route
+function App() {
+  if (!window.location.hash.includes("#confirmation_token=")) {
+    window.location.href = "/confirmation";
+  }
+  return (
+    <Router>
+      <div>
+        <Header />
+        <div id="spacer" />
+        <Switch>
+          <Route exact path="/" component={IndexPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route path="/invite" component={InvitePage} />
+          <Route path="/confirmation" component={ConfirmationPage} />
+          <Route exact path="/about-us/vision" component={VisionPage} />
+          <Route exact path="/about-us/team" component={TeamPage} />
+          <Route
+            exact
+            path="/about-us/constitution"
+            component={ConstitutionPage}
+          />
+          <Route exact path="/schools" component={SchoolPage} />
+          <Route exact path="/parents" component={ParentPage} />
+          <Route exact path="/blog" component={BlogPage} />
+          <Route exact path="/get-involved" component={GetInvolvedPage} />
+          <Route exact path="/donate" component={DonatePage} />
+          <Route exact path="/old-booking" component={PresentationBooking} />
+          <Route exact path="/booking" component={PresentationBookingWizard} />
+          <Route exact path="/contact-us" component={ContactUsPage} />
+          <Route exact path="/volunteer" component={VolunteerPage} />
+          <Route exact path="/faq" component={NotFoundPage} />
+          <Route path="/password-reset" component={PasswordResetPage} />
+          <Route path="/recover" component={PasswordRecoveryPage} />
+          {/* <Route
               path="/presentations/drug-overview"
               component={DrugOverviewPage}
             /> */}
-            <Route path="*" component={NotFoundPage} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
+          <Route path="*" component={NotFoundPage} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 App.propTypes = {
